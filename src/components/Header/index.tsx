9579f2503e8b98b40e193bffd2985e3d15485003
z.shmaria <z.shmaria@auto-i-dat.ch>
2021-04-10 21:33:04 +0200

    Preapred code for ssr builds

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -15,14 +15,23 @@ interface Props{
 
 const Header: React.FC<Props> = ({ siteTitle, display, onToggle }) =>
 {
-  const shouldAnimate :boolean = samePlace();
+  const isBrowser :boolean = typeof window !== "undefined"
+  let shouldAnimate :boolean = false;
+  let windowWidth: number = 1000;
+  if(isBrowser)
+  {
+    shouldAnimate = samePlace();
+    windowWidth = window.innerWidth;
+    
+  }
+  
 
   return (
     <Styled.Header>
       <Styled.Wrapper
         display = {display}
         shouldAnimate = {shouldAnimate}
-        animate={shouldAnimate ? { maxWidth: display == Display.LEFT ? [768,window.innerWidth] : [window.innerWidth, 768] } : false}
+        animate={shouldAnimate ? { maxWidth: display == Display.LEFT ? [768,windowWidth] : [windowWidth, 768] } : false}
         transition={{ ease: display == Display.LEFT ? "anticipate" : "backOut", duration: 1.25 }} >
         <Logo />
         <MainNav onToggle={onToggle}/>
@@ -41,7 +50,7 @@ export default Header;
 
 function samePlace() : boolean {
   const cur  = window.location.pathname;
-  const prev  = (window as any).PreviousPath;
+  const prev = (window as any).PreviousPath;
 
   return (prev === undefined) || (cur && prev && cur.split('/')[1] !== prev.split('/')[1]);
 }
