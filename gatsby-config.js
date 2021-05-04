411c4a3591640013eb75bbdd396ecbecb39464e8
z.shmaria <z.shmaria@auto-i-dat.ch>
2021-05-05 00:40:22 +0200

    Added Google Analystics and new picture

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,27 @@ module.exports = {
     themeColor: `#111827`
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-PJ5VE73EW2" // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true
+        },
+      },
+    },
     `gatsby-plugin-typescript`,
     {
       resolve: `@rocketseat/gatsby-theme-docs`,
